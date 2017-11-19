470f5d552f8bcbc7f4aa26047009cfd771450b57
weineel <LiJF_wn@163.com>
2017-11-19 15:35:00 +0800

    数字字符串改写为千分位显示

---
+++ test/comdify-test.js
@@ -0,0 +1,24 @@
+// import { comdify } from '../comdify'
+// import { assert } from 'chai'
+var comdify = require('../comdify').comdify
+var assert = require('chai').assert
+
+
+describe('把数字字符串改写成千分位', () => {
+    it('123改写为千分为', () => {
+        let v = '123'
+        assert.equal(comdify(v), v)
+    })
+    it('1234改写为千分为', () => {
+        let v = '1234'
+        assert.equal(comdify(v), '1,234')
+    })
+    it('123456改写为千分为', () => {
+        let v = '123456'
+        assert.equal(comdify(v), '123,456')
+    })
+    it('1234567改写为千分为', () => {
+        let v = '1234567'
+        assert.equal(comdify(v), '1,234,567')
+    })
+})
