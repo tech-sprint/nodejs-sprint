3601e2ec243619d0bd3c8490334679c6ef3eb267
weineel <LiJF_wn@163.com>
2018-11-12 21:26:01 +0800

    feat(promise):

--- promise/promise-h.js
+++ promise/promise-h.js
@@ -6,8 +6,63 @@ function ac() {
   })
 }
 
-ac().then(data => {
-  console.log(data)
-})
+const axio = {
+  get(url, params) {
+    console.log(url, params)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const n = Math.random()
+        if (n < 0.5) {
+          resolve({
+            name: n
+          })
+        } else {
+          reject(new Error(n))
+        }
+      }, 500);
+    })
+  }
+}
+
+const result = axio.get('/user/3', {})
+
+async function a1() {
+  try {
+    const data = await result
+    console.log(1, data)
+  } catch (ex) {
+    console.log(2, ex)
+  }
+  return 88
+}
+
+console.log(4444)
+
+// const p1 = Promise.resolve(Promise.reject())
+// console.warn(p1)
+// p1.then(d => {
+//   console.log(2, d)
+// }).catch(e => {
+//   console.log(4, e)
+// })
+
+// console.warn(result)
+// result.then(data => {
+//   console.log(1, data)
+//   // Promise.resolve(...)
+// }).then(data => {
+//   console.log(2, data)  //
+// }).catch(err => {
+//   console.log(3, err)
+// }).then(data => {
+//   console.log(4, data)
+// })
+
+
+
+
+// ac().then(data => {
+//   console.log(data)
+// })
 ///
 console.log('---end---')
---
+++ reflect/index.js
@@ -0,0 +1,3 @@
+// 自省（反射）
+// 反射是一个非常有用的集合，它囊括了所有 JavaScript 引擎内部专有的 “内部方法”，现在被暴露为了一个单一、方便的对象 —— Reflect。
+// 而不再散落在Object.keys、Object.getOwnPropertyNames、Object.prototype.hasOwnProperty等地方。这些方法的列表，https://es5.github.io/#x8.12，https://www.ecma-international.org/ecma-262/6.0/index.html#sec-ordinary-object-internal-methods-and-internal-slots
